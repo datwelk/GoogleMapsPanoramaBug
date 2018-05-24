ae2a821cd1b6e5ecc8a9a6bcc78e2585df766712
Damiaan Twelker <datwelk@me.com>
2018-05-24 16:12:15 +0200

    Initial Commit

---
+++ PanoramaViewCrash/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PanoramaViewCrash
+//
+//  Created by Damiaan Twelker on 24/05/2018.
+//  Copyright © 2018 Damiaan Twelker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PanoramaViewCrash/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PanoramaViewCrash
+//
+//  Created by Damiaan Twelker on 24/05/2018.
+//  Copyright © 2018 Damiaan Twelker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
